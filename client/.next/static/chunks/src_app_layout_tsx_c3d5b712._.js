b9facceb9b52263fef94c46093c34b63b0eb30ed
Sulaimon Omotosho <susudef2u@gmail.com>
2025-08-13 15:34:31 +0100

    Feat: Created Navbar

---
+++ client/.next/static/chunks/src_app_layout_tsx_c3d5b712._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
+  "static/chunks/4ee35_next_dist_9995aa35._.js"
+],
+    source: "dynamic"
+});
